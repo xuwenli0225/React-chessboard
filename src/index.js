e9c15ff57f623d1cb3c2ddf3a40d6c4d70e3fc90
xuwenli <xuwenli0225.com>
2020-06-05 09:15:12 +0800

    井字棋简易版

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './style.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Cell = function (props) {
+  return (
+    <div className="cell" onClick={props.onClick}>
+      {props.text}
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Chessboard = function () {
+  const [cells, setCells] = useState([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ])
+  const [n, setN] = useState(0)
+  const [finished, setFinished] = useState(false)
+  const tell = (cells) => {
+    for (let i = 0; i < 3; i++) {
+      if (cells[i][0] === cells[i][1] && cells[i][1] === cells[i][2] && cells[i][0] !== null) {
+        console.log(cells[i][0] + '赢了')
+        setFinished(true)
+        break;
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (cells[0][i] === cells[1][i] && cells[1][i] === cells[2][i] && cells[0][i] !== null) {
+        console.log(cells[0][i] + '赢了')
+        setFinished(true)
+        break;
+      }
+    }
+    if (cells[0][2] === cells[1][1] && cells[1][1] === cells[2][0] && cells[1][1] !== null) {
+      console.log(cells[1][1] + '赢了')
+      setFinished(true)
+    }
+    if (cells[0][0] === cells[1][1] && cells[1][1] === cells[2][2] && cells[0][0] !== null) {
+      console.log(cells[0][0] + '赢了')
+      setFinished(true)
+    }
+  }
+  const onClickCell = (row, col) => {
+    setN(n + 1)
+    const copy = JSON.parse(JSON.stringify(cells))
+    copy[row][col] = n % 2 === 0 ? 'x' : 'o'
+    setCells(copy)
+    tell(copy)
+  }
+  return (
+    <div>
+      <div>n:{n}</div>
+      {cells.map((items, row) => <div className="row">
+        {items.map((item, col) => <div className="col">
+          <Cell text={item} onClick={() => onClickCell(row, col)} />
+        </div>)}
+        {finished && <div className="gameOver">游戏结束</div>}
+      </div>)}
+    </div>
+  )
+}
+ReactDOM.render(<div>
+  <Chessboard />
+</div>, document.getElementById('root'))
+
